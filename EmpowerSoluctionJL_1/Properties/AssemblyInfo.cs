3211cf51701dc5c236b619182eeb65c15a01c21c
joolivei <joolivei@WPA09GY82ZM3N>
2023-05-30 15:56:44 -0300

    Adicionar arquivos de projeto.

---
+++ EmpowerSoluctionJL_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("EmpowerSoluctionJL_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("EmpowerSoluctionJL_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("8CD42CAD-9E01-4788-83C4-938760694B84")]
+[assembly: AssemblyVersion("1.0.0.0")]
